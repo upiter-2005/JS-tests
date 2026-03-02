2d1bd09d1a128697a3271fd7366072bdacbce71a
upiter-2005 <pavel470245@gmail.com>
2026-03-02 21:51:13 +0200

    added 4.sleep.test & 6.flatten.test

--- src/9.isEqual.ts
+++ src/9.isEqual.ts
@@ -1,11 +1,20 @@
+export {};
+
+declare global {
+  interface Array<T> {
+    isEqual(array: T[]): boolean;
+  }
+}
+
 Array.prototype.isEqual = function (array) {
   if (!Array.isArray(array) || array.length !== this.length) return false;
+
   for (let i = 0; i < this.length; i++) {
     if (this[i] !== array[i]) return false;
   }
   return true;
 };
 
-const arr = [6, 7, "8"];
+// const arr = [6, 7, "8"];
 
-console.log(arr.isEqual([6, 7, "8"]));
+// console.log(arr.isEqual([6, 7, "8"]));
