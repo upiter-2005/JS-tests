22606cabfecec7ce88bcf0e24062d231c1400547
upiter-2005 <pavel470245@gmail.com>
2026-03-03 21:31:21 +0200

    added tests in 17.once.test.ts

--- src/17.memoize.ts
+++ src/17.memoize.ts
@@ -1,34 +1,13 @@
-function memoize<T, U>(fn: T): U {
+export function memoize<T, U>(fn: T): (arg?: U) => U {
   let cache = null;
   let prevValue;
 
   return function (x) {
     if (!cache || prevValue !== x) {
       prevValue = x;
-      cache = fn.apply(this, x);
-      console.log("new call");
+      cache = fn(x);
+      console.log(`new call ${x}`);
     }
     return cache;
   };
 }
-
-const slowFunction = (x) => x * 2;
-
-const memoized = memoize(slowFunction);
-
-console.log(memoized(5));
-console.log(memoized(5));
-console.log(memoized(5));
-console.log(memoized(5));
-console.log(memoized(5));
-console.log(memoized(5));
-console.log(memoized(2));
-console.log(memoized(2));
-console.log(memoized(3));
-console.log(memoized(2));
-
-function test<T, U>(args: T): Array<U> {
-  return [args];
-}
-
-test([5, 6, 7]);
