7fd690a001bf8cd86dbfb0406e5bc1435415463b
upiter-2005 <pavel470245@gmail.com>
2026-03-04 12:10:21 +0200

    added test to mybind functions

--- src/3.myBind.ts
+++ src/3.myBind.ts
@@ -1,20 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export {};
 
 declare global {
   interface Function {
-    myBind(fn: () => void): () => void;
+    myBind(context: any): (...args: any[]) => void | any;
   }
 }
 
 Function.prototype.myBind = function (context) {
-  return () => {
-    this.apply(context);
+  return (...args) => {
+    this.apply(context, args);
   };
 };
 
-// function foo() {
-//   console.log(`foo console.log ` + this.a);
-// }
+function foo() {
+  console.log(`foo console.log ` + this.a);
+}
 
-// const foo1 = foo.myBind({ a: 54 });
-// foo1();
+const foo1 = foo.myBind({ a: 54 });
+foo1();
