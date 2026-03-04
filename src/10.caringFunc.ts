233054e3fbad61fe482f6421d5dec2dc78d6a786
upiter-2005 <pavel470245@gmail.com>
2026-03-04 11:50:09 +0200

    added test to sum  functions

--- src/10.caringFunc.ts
+++ src/10.caringFunc.ts
@@ -1,33 +1,7 @@
-function sum(a) {
-  return function (b) {
-    return function (c) {
+export function sum(a: number) {
+  return function (b: number) {
+    return function (c: number): number {
       return a + b + c;
     };
   };
 }
-console.log(sum(1)(2)(3));
-
-////////////-----------Advanced level---------------///////////////////////
-// function sum(a){
-//   let total = 0;
-
-//   function inner(b){
-//     if(b !== undefined){
-//       total += b;
-//       return inner;
-//     }
-//     return total;
-//   }
-//    inner.valueOf = () => total;
-//    inner.toString = () => String(total);
-//   return inner;
-// }
-// console.log(+sum(1)(2)(3))
-
-// function foo (){
-//   return 777;
-// }
-
-// foo.valueOf = () => 3131
-
-// console.log(foo());
