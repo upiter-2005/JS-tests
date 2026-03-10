71b4f19af9fe48f20f837718986d6bb8d0a27458
Yevhen Poliakov <yevhenpoliakov@MacBook-Pro-Yevhen.local>
2026-03-10 15:53:33 +0200

    push with fail test

--- src/2.sayHi.test.ts
+++ src/2.sayHi.test.ts
@@ -2,6 +2,8 @@ import "./2.sayHi.ts";
 
 describe("sayHi", () => {
   test("should concat text", () => {
-    expect("someString".sayHi("Pavel")).toBe("Hello from: Pavel - someString");
+    expect("someString".sayHi("Pavel")).toBe(
+      "Hello from: Pavel - someString!!!",
+    );
   });
 });
